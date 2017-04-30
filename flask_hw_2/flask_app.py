f801313863ec95ee649cd171c7a5568089fee886
phuuda <phuuda@gmail.com>
2017-04-30 23:13:32 +0300

    initial commits + edits

--- flask_hw_2/flask_app.py
+++ flask_hw_2/flask_app.py
@@ -3,8 +3,8 @@
 from flask import Flask
 from flask import url_for, render_template, request, redirect
 from pymystem3 import Mystem
-import operator
 from collections import Counter
+import requests, operator, json
 
 m = Mystem()
 app = Flask(__name__)
@@ -61,7 +61,17 @@ def ordered_v_lexems(verbs):
     return(just_lex)
 
 
+# enter 2 open group ids
+# return # of people in each group
+# return # of people in both groups
+# if closed group - return that
+
+
 @app.route('/', methods=['get', 'post'])
+def main_one():
+    return render_template('index_page.html')
+
+@app.route('/verb_page', methods=['get', 'post'])
 def index():
     if request.form:
         text = request.form['text']
@@ -81,12 +91,46 @@ def index():
         
         ordered_lexems = ordered_v_lexems(verbs)
             
-        return render_template('index_page.html', input=text, text=pos_text,
+        return render_template('verb_page.html', input=text, text=pos_text,
                                v_quantity=v_quantity, v_percent=v_percent, t_verb_count=t_verb_count,
                                intrans_verb_count=intrans_verb_count, nes_v_count=nes_v_count,
                                sov_v_count=sov_v_count, ordered_lexems=ordered_lexems)
 
-    return render_template('index_page.html')
+    return render_template('verb_page.html')
+
+@app.route('/vk_page', methods=['get', 'post'])
+
+def vk_api():
+    if request.form:
+        client_id = '6010833'
+        client_secret = 'WpWi6xJ68d73LwqSmHqU'
+        grant_type = 'client_credentials'
+
+        auth = 'https://oauth.vk.com/access_token?client_id='
+
+        auth_response = requests.get(auth + client_id + '&client_secret=' + client_secret +
+                                     '&v=5.63&grant_type=client_credentials')
+
+        m = json.loads(auth_response.text)
+        access_token = m['access_token']
+
+
+        group_one = request.args['group_one']
+        group_two = request.args['group_two']
+
+        gr1_response = requests.get(api_link + method + '?id=' + group_one +
+                            '&client_secret=' + access_token)
+
+        gr2_response = requests.get(api_link + method + '?id=' + group_two +
+                            '&client_secret=' + access_token)    
+
+        group1 = gr1_response.text
+        group2 = gr2_response.text
+
+    
+        return render_template('vk_page.html', group1=group1, group2=group2)
+    
+    return render_template('vk_page.html')    
 
 if __name__ == '__main__':
     app.run(debug=True)
